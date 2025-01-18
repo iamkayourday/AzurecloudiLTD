70c8e84be1ff9a2721b56e78ff58823ed8167926
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2025-01-18 22:04:34 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Header from './components/Header';
 import Home from './Pages/Home';
 import AboutUs from './Pages/AboutUs';
 import Services from './components/Services';
+import CaseStudies from './Pages/CaseStudies';
+import Careers from './Pages/Careers';
+import ContactUs from './Pages/Contactus';
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about-us" element={<AboutUs />} />
           <Route path="/services" element={<Services />} />
+          <Route path="/portfolio" element={<CaseStudies />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/contact-us" element={<ContactUs />} />
 
         </Routes>
         <Footer />
