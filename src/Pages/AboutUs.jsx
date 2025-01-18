976cf8d8d86f0a756ef4b782e7840dff0cfd7514
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2025-01-18 23:28:14 +0100

    Add images

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,97 +1,227 @@
-import React from 'react';
-import { FaAws, FaMicrosoft, FaServer } from 'react-icons/fa';
+import React from "react";
+import { motion } from "framer-motion";
+import { FaAws, FaMicrosoft, FaServer } from "react-icons/fa";
 
-const teamMembers = [
-  { name: 'John Doe', role: 'CEO', image: 'https://via.placeholder.com/150' },
-  { name: 'Jane Smith', role: 'CTO', image: 'https://via.placeholder.com/150' },
-  { name: 'James Brown', role: 'COO', image: 'https://via.placeholder.com/150' },
-];
+// const teamMembers = [
+//   { name: "John Doe", role: "CEO", image: "/team1.jpg" },
+//   { name: "Jane Smith", role: "CTO", image: "/team2.jpg" },
+//   { name: "Robert Brown", role: "CFO", image: "/team3.jpg" },
+//   { name: "Emily White", role: "COO", image: "/team4.jpg" }
+// ];
 
 const CertificationCard = ({ Icon, text, color }) => (
-  <div className="flex items-center space-x-4 hover:scale-105 transition-transform">
+  <motion.div
+    whileHover={{ scale: 1.1 }}
+    whileTap={{ scale: 0.9 }}
+    className="flex items-center space-x-4 hover:scale-105 transition-transform"
+  >
     <Icon className={`w-12 h-12 ${color}`} />
     <span className="text-lg text-gray-600">{text}</span>
-  </div>
+  </motion.div>
 );
 
 const AboutUs = () => {
   return (
-    <div className="container mx-auto px-6 py-16 mt-10 space-y-16">
+    <div className="container mx-auto px-6 py-16 mt-16 space-y-16">
       {/* Page Heading */}
       <header className="text-center mb-16">
-        <h1 className="text-5xl font-extrabold text-gray-800">
+        <motion.h1
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          className="text-5xl font-extrabold text-[#517db9]"
+        >
           About Us
-        </h1>
-        <p className="text-lg text-gray-600 mt-4">
-          Get to know our journey, mission, and the incredible team behind our success.
-        </p>
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.2, duration: 0.8 }}
+          className="text-lg text-gray-600 mt-4"
+        >
+          Get to know our journey, mission, and the incredible team behind our
+          success.
+        </motion.p>
       </header>
 
       {/* Mission and Vision Statement */}
       <section className="flex flex-col md:flex-row items-center space-y-6 md:space-y-0">
-        <div className="md:w-1/2">
-          <h2 className="text-3xl font-extrabold text-gray-800 mb-4">Our Mission and Vision</h2>
+        <motion.div
+          className="md:w-1/2"
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          <h2 className="text-3xl font-extrabold text-[#517db9] mb-4">
+            Our Mission and Vision
+          </h2>
           <p className="text-lg text-gray-600 leading-relaxed">
-            We are committed to empowering innovation and collaboration through technology. Our vision is to
-            create a world where technology fosters global connectivity and bridges gaps.
+            At our core, we are driven by a mission to foster innovation and
+            global connectivity through cutting-edge technology. We aim to
+            create solutions that bring people together, bridge cultural and
+            geographical gaps, and empower communities worldwide. Our vision is
+            a future where technology transcends boundaries, inspires progress,
+            and shapes a more inclusive world.
           </p>
-        </div>
-        <div className="md:w-1/2">
+        </motion.div>
+        <motion.div
+          className="md:w-1/2"
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8 }}
+        >
           <img
             src="/Hero.webp"
             alt="Team collaborating on a project"
             className="w-full rounded-lg shadow-md h-80"
           />
-        </div>
+        </motion.div>
       </section>
 
       {/* Company Story */}
       <section className="flex flex-col md:flex-row-reverse items-center space-y-6 md:space-y-0">
-        <div className="md:w-1/2 lg:ml-5">
-          <h2 className="text-3xl font-extrabold text-gray-800 mb-4">Our Story</h2>
+        <motion.div
+          className="md:w-1/2 lg:ml-5"
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          <h2 className="text-3xl font-extrabold text-[#517db9] mb-4">
+            Our Story
+          </h2>
           <p className="text-lg text-gray-600 leading-relaxed">
-            From humble beginnings to a global partner, our journey has been driven by a passion for innovation.
-            We've transformed challenges into opportunities, always staying true to our values.
+            Starting as a small team with big dreams, we’ve grown into a global
+            force shaping the technology landscape. Each milestone in our
+            journey is a testament to resilience, innovation, and the relentless
+            pursuit of excellence. With a diverse team and a collaborative
+            spirit, we continue to rewrite the narrative of what’s possible in
+            the tech world.
           </p>
-        </div>
-        <div className="md:w-1/2">
+          <p className="text-lg text-gray-600 mt-4">
+            Together, we’ve built partnerships, tackled challenges, and
+            celebrated achievements that inspire us to keep striving for greater
+            heights.
+          </p>
+        </motion.div>
+        <motion.div
+          className="md:w-1/2"
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8 }}
+        >
           <img
             src="/Hero.webp"
             alt="Our journey and success"
             className="w-full rounded-lg shadow-md h-80"
           />
-        </div>
+        </motion.div>
       </section>
 
       {/* Team Members Section */}
       <section>
-        <h2 className="text-3xl font-extrabold text-gray-800 mb-8 text-center">Meet Our Team</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {teamMembers.map((member, index) => (
-            <div
-              key={index}
-              className="bg-white p-6 rounded-lg shadow-lg text-center hover:shadow-xl transition-shadow"
-            >
-              <img
-                src={member.image}
-                alt={`Portrait of ${member.name}`}
-                className="w-32 h-32 rounded-full mx-auto mb-4"
-              />
-              <h3 className="text-xl font-bold text-gray-800">{member.name}</h3>
-              <p className="text-gray-600">{member.role}</p>
+        <motion.h2
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          className="text-3xl font-extrabold text-[#517db9] mb-8 text-center"
+        >
+          Meet Our Team
+        </motion.h2>
+        <motion.div
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.2, duration: 0.8 }}
+        >
+          {/* Individual Team Member 1 */}
+          <div className="relative group p-6 rounded-full w-80 h-96 mx-auto overflow-hidden transition-shadow">
+            {/* Team Member Image */}
+            <img
+              src="/team1.jpg"
+              alt="Portrait of John Doe"
+              className="w-full h-full object-cover rounded-full"
+            />
+            {/* Hover Content */}
+            <div className="absolute bottom-0 w-full flex flex-col items-center justify-center bg-[#f0f8ff] opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-b-full p-4">
+              <h3 className="text-xl font-bold text-gray-800">Jane Doe</h3>
+              <p className="text-gray-600">CEO</p>
             </div>
-          ))}
-        </div>
+          </div>
+
+          {/* Individual Team Member 2 */}
+          <div className="relative group p-6 rounded-full w-80 h-96 mx-auto overflow-hidden transition-shadow">
+            {/* Team Member Image */}
+            <img
+              src="/team2.jpg"
+              alt="Portrait of Jane Smith"
+              className="w-full h-full object-cover rounded-full"
+            />
+            {/* Hover Content */}
+            <div className="absolute bottom-0 w-full flex flex-col items-center justify-center bg-[#f0f8ff] opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-b-full p-4">
+              <h3 className="text-xl font-bold text-gray-800">Jane Smith</h3>
+              <p className="text-gray-600">CTO</p>
+            </div>
+          </div>
+
+          {/* Individual Team Member 3 */}
+          <div className="relative group p-6 rounded-full w-80 h-96 mx-auto overflow-hidden transition-shadow">
+            {/* Team Member Image */}
+            <img
+              src="/team3.jpg"
+              alt="Portrait of Robert Brown"
+              className="w-full h-full object-cover rounded-full"
+            />
+            {/* Hover Content */}
+            <div className="absolute bottom-0 w-full flex flex-col items-center justify-center bg-[#f0f8ff] opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-b-full p-4">
+              <h3 className="text-xl font-bold text-gray-800">Robert Brown</h3>
+              <p className="text-gray-600">CFO</p>
+            </div>
+          </div>
+
+          {/* Individual Team Member 4 */}
+          <div className="relative group p-6 rounded-full w-80 h-96 mx-auto overflow-hidden transition-shadow">
+            {/* Team Member Image */}
+            <img
+              src="/team4.jpg"
+              alt="Portrait of Emily White"
+              className="w-full h-full object-cover rounded-full"
+            />
+            {/* Hover Content */}
+            <div className="absolute bottom-0 w-full flex flex-col items-center justify-center bg-[#f0f8ff] opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-b-full p-4">
+              <h3 className="text-xl font-bold text-gray-800">Emily White</h3>
+              <p className="text-gray-600">COO</p>
+            </div>
+          </div>
+        </motion.div>
       </section>
 
       {/* Certifications and Partnerships */}
       <section>
-        <h2 className="text-3xl font-extrabold text-gray-800 mb-8 text-center">Certifications and Partnerships</h2>
-        <div className="flex flex-wrap justify-center gap-8">
-          <CertificationCard Icon={FaMicrosoft} text="Microsoft Partner" color="text-blue-600" />
-          <CertificationCard Icon={FaAws} text="AWS Partner" color="text-yellow-600" />
-          <CertificationCard Icon={FaServer} text="Industry Certified" color="text-green-600" />
-        </div>
+        <h2 className="text-3xl font-extrabold text-gray-800 mb-8 text-center">
+          Certifications and Partnerships
+        </h2>
+        <motion.div
+          className="flex flex-wrap justify-center gap-8"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8 }}
+        >
+          <CertificationCard
+            Icon={FaMicrosoft}
+            text="Microsoft Partner"
+            color="text-blue-600"
+          />
+          <CertificationCard
+            Icon={FaAws}
+            text="AWS Partner"
+            color="text-yellow-600"
+          />
+          <CertificationCard
+            Icon={FaServer}
+            text="Industry Certified"
+            color="text-green-600"
+          />
+        </motion.div>
       </section>
     </div>
   );
--- src/Pages/Contactus.jsx
+++ src/Pages/Contactus.jsx
@@ -1,41 +1,47 @@
 import React from "react";
 import { FaUser, FaEnvelope, FaPhoneAlt, FaCommentDots } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const ContactUs = () => {
   return (
-    <div className="container mx-auto px-6 py-16">
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-16">
       {/* Header Section */}
-      <header className="text-center mb-16 mt-14">
-        <h2 className="text-5xl font-extrabold text-gray-800">Contact Us</h2>
-        <p className="text-lg text-gray-600 mt-4">
+      <header className="text-center mb-12">
+        <h2 className="text-4xl sm:text-5xl font-extrabold text-gray-800">
+          Contact Us
+        </h2>
+        <p className="text-base sm:text-lg text-gray-600 mt-4">
           We’d love to hear from you. Whether you have a question or just want
           to say hello, feel free to reach out!
         </p>
       </header>
 
       {/* Contact Form and Image Section */}
-      <section className="flex flex-col lg:flex-row items-stretch lg:space-x-8 mb-16">
+      <section className="flex flex-col lg:flex-row items-stretch lg:space-x-8">
         {/* Image */}
-        <div className="lg:w-1/2 w-full mb-8 lg:mb-0 flex items-center border-2 border-[#517db9] rounded-lg">
-          <img
-            src="Hero.webp"
-            alt="Contact Us"
-            className="rounded-lg shadow-lg w-full h-full object-cover"
-          />
-        </div>
+        <Link to="/" className="lg:w-1/2 w-full mb-8 lg:mb-0">
+          <div className="h-64 sm:h-80 lg:h-full flex items-center border-2 border-blue-500 rounded-lg overflow-hidden">
+            <img
+              src="Hero.webp"
+              alt="Contact Us"
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </Link>
 
         {/* Form */}
-        <div className="lg:w-1/2 w-full border-2 border-[#517db9] rounded-lg">
-          <form className="bg-white p-6 rounded-lg shadow-lg h-full flex flex-col space-y-6">
-            <h3 className="text-3xl font-extrabold text-gray-800 mb-6">
+        <div className="lg:w-1/2 w-full">
+          <form className="bg-white p-6 sm:p-8 rounded-lg shadow-lg flex flex-col space-y-6">
+            <h3 className="text-2xl sm:text-3xl font-extrabold text-gray-800">
               Get in Touch
             </h3>
 
-            <div className="grid grid-cols-2 gap-4">
-              <div className="relative">
+            {/* Name Fields */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+              <div>
                 <label
                   htmlFor="first-name"
-                  className="block text-gray-700 font-medium mb-2"
+                  className="block text-sm font-medium text-gray-700 mb-2"
                 >
                   First Name
                 </label>
@@ -45,14 +51,14 @@ const ContactUs = () => {
                     type="text"
                     id="first-name"
                     placeholder="First Name"
-                    className="w-full border-0 rounded-r-lg p-3 focus:outline-none focus:ring focus:ring-blue-300"
+                    className="w-full border-0 p-3 rounded-r-lg focus:outline-none focus:ring focus:ring-blue-300"
                   />
                 </div>
               </div>
-              <div className="relative">
+              <div>
                 <label
                   htmlFor="last-name"
-                  className="block text-gray-700 font-medium mb-2"
+                  className="block text-sm font-medium text-gray-700 mb-2"
                 >
                   Last Name
                 </label>
@@ -62,16 +68,17 @@ const ContactUs = () => {
                     type="text"
                     id="last-name"
                     placeholder="Last Name"
-                    className="w-full border-0 rounded-r-lg p-3 focus:outline-none focus:ring focus:ring-blue-300"
+                    className="w-full border-0 p-3 rounded-r-lg focus:outline-none focus:ring focus:ring-blue-300"
                   />
                 </div>
               </div>
             </div>
 
-            <div className="relative">
+            {/* Email Field */}
+            <div>
               <label
                 htmlFor="email"
-                className="block text-gray-700 font-medium mb-2"
+                className="block text-sm font-medium text-gray-700 mb-2"
               >
                 Email
               </label>
@@ -81,15 +88,16 @@ const ContactUs = () => {
                   type="email"
                   id="email"
                   placeholder="Your Email"
-                  className="w-full border-0 rounded-r-lg p-3 focus:outline-none focus:ring focus:ring-blue-300"
+                  className="w-full border-0 p-3 rounded-r-lg focus:outline-none focus:ring focus:ring-blue-300"
                 />
               </div>
             </div>
 
-            <div className="relative">
+            {/* Phone Field */}
+            <div>
               <label
                 htmlFor="phone"
-                className="block text-gray-700 font-medium mb-2"
+                className="block text-sm font-medium text-gray-700 mb-2"
               >
                 Phone Number
               </label>
@@ -100,15 +108,16 @@ const ContactUs = () => {
                   id="phone"
                   placeholder="Phone Number"
                   pattern="[0-9+\-\(\) ]*"
-                  className="w-full border-0 rounded-r-lg p-3 focus:outline-none focus:ring focus:ring-blue-300"
+                  className="w-full border-0 p-3 rounded-r-lg focus:outline-none focus:ring focus:ring-blue-300"
                 />
               </div>
             </div>
 
-            <div className="relative">
+            {/* Message Field */}
+            <div>
               <label
                 htmlFor="message"
-                className="block text-gray-700 font-medium mb-2"
+                className="block text-sm font-medium text-gray-700 mb-2"
               >
                 Message
               </label>
@@ -118,11 +127,12 @@ const ContactUs = () => {
                   id="message"
                   placeholder="Your Message"
                   rows="4"
-                  className="w-full border-0 rounded-r-lg p-3 focus:outline-none focus:ring focus:ring-blue-300"
+                  className="w-full border-0 p-3 rounded-r-lg focus:outline-none focus:ring focus:ring-blue-300"
                 ></textarea>
               </div>
             </div>
 
+            {/* Submit Button */}
             <button
               type="submit"
               className="w-full bg-blue-600 text-white font-semibold py-3 rounded-lg hover:bg-blue-700 transition-colors"
@@ -134,8 +144,8 @@ const ContactUs = () => {
       </section>
 
       {/* Text Under the Form and Image */}
-      <div className="text-center mb-16">
-        <p className="text-lg text-gray-600">
+      <div className="text-center mt-12">
+        <p className="text-sm sm:text-lg text-gray-600">
           We’d love to hear from you or just reach out manually to{" "}
           <a href="mailto:info@example.com" className="text-blue-600">
             info@example.com
@@ -144,9 +154,9 @@ const ContactUs = () => {
       </div>
 
       {/* Grid of Contact Options */}
-      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 text-center">
+      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
         <div className="bg-white p-6 rounded-lg shadow-lg">
-          <h4 className="text-xl font-bold text-gray-800">Email Support</h4>
+          <h4 className="text-lg font-bold text-gray-800">Email Support</h4>
           <p className="text-gray-600 mt-2">
             Reach us at{" "}
             <a href="mailto:support@example.com" className="text-blue-600">
@@ -155,13 +165,13 @@ const ContactUs = () => {
           </p>
         </div>
         <div className="bg-white p-6 rounded-lg shadow-lg">
-          <h4 className="text-xl font-bold text-gray-800">Visit Our Office</h4>
+          <h4 className="text-lg font-bold text-gray-800">Visit Our Office</h4>
           <p className="text-gray-600 mt-2">
             123 Main Street, City, State, ZIP
           </p>
         </div>
         <div className="bg-white p-6 rounded-lg shadow-lg">
-          <h4 className="text-xl font-bold text-gray-800">Call Us Directly</h4>
+          <h4 className="text-lg font-bold text-gray-800">Call Us Directly</h4>
           <p className="text-gray-600 mt-2">Phone: +1 (123) 456-7890</p>
         </div>
       </section>
